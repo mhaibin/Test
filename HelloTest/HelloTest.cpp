ebac045e11c8f0b087e5f85578222c5aa854fc37
mhaibin <416531936@qq.com>
2017-10-12 14:41:09 +0800

    HelloTest 源码

---
+++ HelloTest/HelloTest.cpp
@@ -0,0 +1,19 @@
+// HelloTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\r\n");
+// 	while(1)
+// 	{
+// 		Sleep(10000);
+// 	}
+//	system("pause");
+	return 0;
+}
+
